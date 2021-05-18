0d9c4e8b96e52f6eda75ecb84054472fddcd0755
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-05-17 19:41:17 -0700

    finalTest!!!!!

--- caesar.py
+++ caesar.py
@@ -6,8 +6,9 @@ alphaBet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 def encrypt(key, plaintext):
     ciphertext = ""
     for char in plaintext:
-        char_index = (ord(char) - ord('A') + 26 - key%26) % 26
+        char_index = (ord(char) - ord('A') + 26 - key % 26) % 26
         ciphertext += alphaBet[char_index]
+    # print(ciphertext)
     return ciphertext
 
 
@@ -15,5 +16,17 @@ def decrypt(key, ciphertext):
     plaintext = ""
     for char in ciphertext:
         char_index = (ord(char) - ord('A') + key) % 26
-        ciphertext += alphaBet[char_index]
+        plaintext += alphaBet[char_index]
+    # print(plaintext)
     return plaintext
+
+# 
+# encrypt(3, "ABC")
+# decrypt(3, "XYZ")
+
+# def main():
+#     print("Hello World!")
+#
+#
+# if __name__ == "__main__":
+#     main()
