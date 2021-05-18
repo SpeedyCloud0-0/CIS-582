76b1f438bdde007be47d69a3406fe55c773d0c30
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-05-17 19:30:08 -0700

    test3

--- caesar.py
+++ caesar.py
@@ -1,11 +1,12 @@
-charMap = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
-alphaBet = ["A", "B", "C", "D", "E", "F", "G"]
+# charMap = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
+alphaBet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 
 def encrypt(key, plaintext):
     ciphertext = ""
     for char in plaintext:
-        char_index = (charMap[char] + key % 26) % 26
+        char_index = (ord(char) - ord('A') + 26 - key%26) % 26
         ciphertext += alphaBet[char_index]
     return ciphertext
 
@@ -13,6 +14,6 @@ def encrypt(key, plaintext):
 def decrypt(key, ciphertext):
     plaintext = ""
     for char in ciphertext:
-        char_index = (charMap[char] - key % 26) % 26
+        char_index = (ord(char) - ord('A') + key) % 26
         ciphertext += alphaBet[char_index]
     return plaintext
